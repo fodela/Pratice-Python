7c7018e86f159c0a49dadcc87471566ffdce7a91
fodela <particleme30@gmail.com>
2022-02-16 21:34:39 +0000

    docs: correct typos

--- capitalize.py
+++ capitalize.py
@@ -1,3 +1,4 @@
+import os
 # You are asked to ensure that the first and last names of people begin with a capital letter in their passports. For example, alison heck should be capitalised correctly as Alison Heck.
 
 
